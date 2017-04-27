1ad97fe84e082abf48085fe7351988a1d4fc0050
Ryan Gaus <rsg1egoman@gmail.com>
2017-04-27 08:05:45 -0400

    code: refactor sending message code
    - Pull out drawing reactions into its own fn
    - Fix a bug where the array of users that reacted to a message would be wrong

--- frontend/frontend.go
+++ frontend/frontend.go
@@ -1,10 +1,8 @@
 package frontend
 
 import (
-	"log"
 	"fmt"
 	"regexp"
-	"strings"
 	"github.com/gdamore/tcell"
 	"github.com/kyokomi/emoji" // convert :smile: to unicode
 
@@ -46,6 +44,7 @@ func NewTerminalDisplay(screen tcell.Screen) *TerminalDisplay {
 				Background(tcell.ColorBlue).
 				Foreground(tcell.ColorWhite),
 			"StatusBarConnection": tcell.StyleDefault,
+			"MessageReaction": tcell.StyleDefault,
 		},
 	}
 }
@@ -159,83 +158,6 @@ func (term *TerminalDisplay) DrawCommandBar(
 }
 
 
-
-// Draw message history in the channel
-func (term *TerminalDisplay) DrawMessages(messages []gateway.Message) {
-	width, height := term.screen.Size()
-	log.Println("start")
-
-
-	for r := 0; r < height - bottomPadding; r++ {
-		// Clear the row.
-		for i := 0; i < width; i++ {
-			char, _, style, _ := term.screen.GetContent(i, r)
-			if char != ' ' || style != tcell.StyleDefault {
-				term.screen.SetCell(i, r, tcell.StyleDefault, ' ')
-			}
-		}
-	}
-
-
-	// Loop from the bottom of the window to the top.
-	index := len(messages) - 1
-	row := height - 2
-	for row > 0 && index >= 0 {
-		msg := messages[index]
-
-		// Get the name of the sender, and the sender's color
-		sender := "(anon)"
-		senderStyle := tcell.StyleDefault
-		if msg.Sender != nil {
-			sender = msg.Sender.Name
-			// If the sender has a color associated, use that!
-			if len(msg.Sender.Color) > 0 {
-				senderStyle = senderStyle.Foreground(tcell.GetColor("#" + msg.Sender.Color))
-			}
-		}
-
-		// Calculate how many rows the message requires to render.
-		messageColumnWidth := width - len(sender) - 1
-		messageRows := (len(makePrintWorthy(msg.Text)) / messageColumnWidth) + 1
-		if len(msg.Reactions) > 0 {
-			messageRows += 1
-		}
-		log.Println("Message rows", messageRows)
-
-		// Render the sender and the message
-		for rowDelta, messageRow := range partitionIntoRows(
-			makePrintWorthy(msg.Text), // Our message to render to the screen
-			messageColumnWidth, // The width of each message row
-		) {
-			if rowDelta == 0 {
-				// Draw the sender on the first row of a message
-				term.WriteTextStyle(0, row - messageRows + 1, senderStyle, sender)
-
-				// Render reactions after message
-				if len(msg.Reactions) > 0 {
-					reactionOffset := len(sender) + 1
-					for _, reaction := range msg.Reactions {
-						reactionEmoji := emoji.Sprintf("%d :"+reaction.Name+":", len(reaction.Users))
-						term.WriteText(reactionOffset, row, reactionEmoji)
-						reactionOffset += len(reactionEmoji)
-					}
-				}
-			}
-
-			// Render the message row.
-			term.WriteTextStyle(
-				len(sender) + 1,
-				row - messageRows + rowDelta,
-				tcell.StyleDefault,
-				strings.Trim(messageRow, " "),
-			)
-		}
-
-		// Subtract the message's height.
-		row -= messageRows;
-		index -= 1;
-	}
-}
 
 func (term *TerminalDisplay) WriteText(x int, y int, text string) {
 	term.WriteTextStyle(x, y, tcell.StyleDefault, text)
