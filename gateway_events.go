d861d8e2f1701b2c25c36effcba0df8ca80cb972
Ryan Gaus <rsg1egoman@gmail.com>
2017-05-27 12:09:51 -0400

    code: act on all events recieved on all gateways, not just the active conenction
    Closes #19

--- gateway_events.go
+++ gateway_events.go
@@ -13,162 +13,164 @@ func gatewayEvents(state *State, term *frontend.TerminalDisplay) {
 	cachedUsers := make(map[string]*gateway.User)
 
 	for {
-		if state.ActiveConnection() == nil {
-			time.Sleep(100 * time.Millisecond) // Sleep to lower the speed of the loop for debugging reasons.
-			continue
-		}
-
-		// Before events can run, confirm that the a channel is selected.
-		hasFetchedChannel := state.ActiveConnection().SelectedChannel() != nil
-
-		// Is the channel empty? If so, move to the next ieration.
-		// We want the loop to always be running, so that if the reference that
-		// state.ActiveConnection() points to behind the scenes changes, the we won't be blocking
-		// listening for events on an old reference.
-		if len(state.ActiveConnection().Incoming()) == 0 || !hasFetchedChannel {
-			time.Sleep(100 * time.Millisecond) // Sleep to lower the speed of the loop for debugging reasons.
-			continue
-		}
-
-		// Now that we know there are events, grab one and handle it.
-		event := <-state.ActiveConnection().Incoming()
-		log.Printf("Received event: %+v", event)
-
-		switch event.Type {
-		case "hello":
-			state.ActiveConnection().AppendMessageHistory(gateway.Message{
-				Sender:    nil,
-				Text:      "Got Hello...",
-				Hash:      "hello",
-				Timestamp: int(time.Now().Unix()),
-			})
-
-			// Send an outgoing message
-			state.ActiveConnection().Outgoing() <- gateway.Event{
-				Type: "ping",
-				Data: map[string]interface{}{
-					"foo": "bar",
-				},
+		for _, conn := range state.Connections {
+			if conn == nil {
+				time.Sleep(100 * time.Millisecond) // Sleep to lower the speed of the loop for debugging reasons.
+				continue
 			}
 
-		case "desktop_notification":
-			if title, ok := event.Data["title"].(string); ok {
-				if content, ok := event.Data["content"].(string); ok {
-					if image, ok := event.Data["avatarImage"].(string); ok {
-						Notification(title, content, image)
+			// Before events can run, confirm that the a channel is selected.
+			hasFetchedChannel := conn.SelectedChannel() != nil
+
+			// Is the channel empty? If so, move to the next ieration.
+			// We want the loop to always be running, so that if the reference that
+			// conn points to behind the scenes changes, the we won't be blocking
+			// listening for events on an old reference.
+			if len(conn.Incoming()) == 0 || !hasFetchedChannel {
+				time.Sleep(100 * time.Millisecond) // Sleep to lower the speed of the loop for debugging reasons.
+				continue
+			}
+
+			// Now that we know there are events, grab one and handle it.
+			event := <-conn.Incoming()
+			log.Printf("Received event: %+v", event)
+
+			switch event.Type {
+			case "hello":
+				conn.AppendMessageHistory(gateway.Message{
+					Sender:    nil,
+					Text:      "Got Hello...",
+					Hash:      "hello",
+					Timestamp: int(time.Now().Unix()),
+				})
+
+				// Send an outgoing message
+				conn.Outgoing() <- gateway.Event{
+					Type: "ping",
+					Data: map[string]interface{}{
+						"foo": "bar",
+					},
+				}
+
+			case "desktop_notification":
+				if title, ok := event.Data["title"].(string); ok {
+					if content, ok := event.Data["content"].(string); ok {
+						if image, ok := event.Data["avatarImage"].(string); ok {
+							Notification(title, content, image)
+						}
 					}
 				}
-			}
 
-		// When a message is received for the selected channel, add to the message history
-		// "message" events come in when the gateway receives a message sent by someone else.
-		case "message":
-			if channel := state.ActiveConnection().SelectedChannel(); event.Data["channel"] == channel.Id {
-				if event.Data["subtype"] == "message_deleted" {
-					// If a message was deleted, then delete the message from the message history
-					for index, msg := range state.ActiveConnection().MessageHistory() {
-						if msg.Hash == event.Data["deleted_ts"] {
-							state.ActiveConnection().DeleteMessageHistory(index)
+			// When a message is received for the selected channel, add to the message history
+			// "message" events come in when the gateway receives a message sent by someone else.
+			case "message":
+				if channel := conn.SelectedChannel(); event.Data["channel"] == channel.Id {
+					if event.Data["subtype"] == "message_deleted" {
+						// If a message was deleted, then delete the message from the message history
+						for index, msg := range conn.MessageHistory() {
+							if msg.Hash == event.Data["deleted_ts"] {
+								conn.DeleteMessageHistory(index)
+							}
+						}
+					} else {
+						// JUST A NORMAL MESSAGE!
+
+						// Find a hash for the message, just use the timestamp
+						// In message events, the timestamp is `ts`
+						// In pong events, the timestamp is `event_ts`
+						var messageHash string
+						if data, ok := event.Data["ts"].(string); ok {
+							messageHash = data
+						} else {
+							log.Fatal("No ts key in message so can't create a hash for this message!")
+						}
+
+						// See if the message is already in the history
+						alreadyInHistory := false
+						for _, msg := range conn.MessageHistory() {
+							if msg.Hash == messageHash {
+								// Message with that hash is already in the history, no need to add
+								// again...
+								alreadyInHistory = true
+								break
+							}
+						}
+						if alreadyInHistory {
+							break
+						}
+
+						message, err := conn.ParseMessage(event.Data, cachedUsers)
+						if err == nil {
+							// Add message to history
+							conn.AppendMessageHistory(*message)
+
+							// If the user that sent the message was typing, they aren't anymore.
+							conn.TypingUsers().Remove(message.Sender.Name)
+						} else {
+							log.Fatalf(err.Error())
 						}
 					}
 				} else {
-					// JUST A NORMAL MESSAGE!
+					log.Printf("Channel value", channel)
+				}
 
-					// Find a hash for the message, just use the timestamp
-					// In message events, the timestamp is `ts`
-					// In pong events, the timestamp is `event_ts`
-					var messageHash string
-					if data, ok := event.Data["ts"].(string); ok {
-						messageHash = data
-					} else {
-						log.Fatal("No ts key in message so can't create a hash for this message!")
-					}
+				// case "reaction_added":
+				//   // If a message was deleted, then delete the message from the message history
+				//   for index, msg := range conn.MessageHistory() {
+				//     if msg.Hash == event.Data["event_ts"] {
+				//       for _, reaction := range msg.Reactions {
+				//         event.Data["reaction"] // == "smile"
+				//         event.Data["item_user"] // == "U0M9S59T2"
+				//       }
+				//     }
+				//   }
 
-					// See if the message is already in the history
-					alreadyInHistory := false
-					for _, msg := range state.ActiveConnection().MessageHistory() {
-						if msg.Hash == messageHash {
-							// Message with that hash is already in the history, no need to add
-							// again...
-							alreadyInHistory = true
-							break
+			// When a user starts typing, then display that they are typing.
+			case "user_typing":
+				if conn != nil {
+					if channel := conn.SelectedChannel(); event.Data["channel"] == channel.Id {
+						if userId, ok := event.Data["user"].(string); ok {
+							user, err := conn.UserById(userId)
+							if err != nil {
+								log.Println(err.Error())
+							} else if user != nil {
+								// Add user to the list of users typing.
+								conn.TypingUsers().Add(user.Name, time.Now())
+							} else {
+								log.Println("User in `user_typing` event was nil, ignoring...")
+							}
+						} else {
+							log.Println("User id in `user_typing` raw event was not coersable to string, ignoring...")
 						}
 					}
-					if alreadyInHistory {
-						break
-					}
-
-					message, err := state.ActiveConnection().ParseMessage(event.Data, cachedUsers)
-					if err == nil {
-						// Add message to history
-						state.ActiveConnection().AppendMessageHistory(*message)
-
-						// If the user that sent the message was typing, they aren't anymore.
-						state.ActiveConnection().TypingUsers().Remove(message.Sender.Name)
-					} else {
-						log.Fatalf(err.Error())
-					}
 				}
-			} else {
-				log.Printf("Channel value", channel)
-			}
 
-			// case "reaction_added":
-			//   // If a message was deleted, then delete the message from the message history
-			//   for index, msg := range state.ActiveConnection().MessageHistory() {
-			//     if msg.Hash == event.Data["event_ts"] {
-			//       for _, reaction := range msg.Reactions {
-			//         event.Data["reaction"] // == "smile"
-			//         event.Data["item_user"] // == "U0M9S59T2"
-			//       }
-			//     }
-			//   }
+			// When the user's presence value changes, update the active connection
+			// {"type":"presence_change","presence":"away","user":"U5FR33U4T"}
+			case "presence_change":
+				if conn != nil {
+					// Get user presence status
+					status := false
+					if value, ok := event.Data["presence"].(string); ok && value == "active" {
+						status = true
+					}
 
-		// When a user starts typing, then display that they are typing.
-		case "user_typing":
-			if state.ActiveConnection() != nil {
-				if channel := state.ActiveConnection().SelectedChannel(); event.Data["channel"] == channel.Id {
+					// Get user instance
 					if userId, ok := event.Data["user"].(string); ok {
-						user, err := state.ActiveConnection().UserById(userId)
+						user, err := conn.UserById(userId)
 						if err != nil {
 							log.Println(err.Error())
-						} else if user != nil {
-							// Add user to the list of users typing.
-							state.ActiveConnection().TypingUsers().Add(user.Name, time.Now())
 						} else {
-							log.Println("User in `user_typing` event was nil, ignoring...")
+							conn.SetUserOnline(user, status)
 						}
-					} else {
-						log.Println("User id in `user_typing` raw event was not coersable to string, ignoring...")
 					}
 				}
+
+			case "":
+				log.Printf("Unknown event received: %+v", event)
 			}
 
-		// When the user's presence value changes, update the active connection
-		// {"type":"presence_change","presence":"away","user":"U5FR33U4T"}
-		case "presence_change":
-			if state.ActiveConnection() != nil {
-				// Get user presence status
-				status := false
-				if value, ok := event.Data["presence"].(string); ok && value == "active" {
-					status = true
-				}
-
-				// Get user instance
-				if userId, ok := event.Data["user"].(string); ok {
-					user, err := state.ActiveConnection().UserById(userId)
-					if err != nil {
-						log.Println(err.Error())
-					} else {
-						state.ActiveConnection().SetUserOnline(user, status)
-					}
-				}
-			}
-
-		case "":
-			log.Printf("Unknown event received: %+v", event)
+			render(state, term)
 		}
-
-		render(state, term)
 	}
 }
